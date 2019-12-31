9c00c8cc5764477aefd560003978bcb3c4201631
Mikael Korpela <mikael@ihminen.org>
2019-12-31 18:30:55 +0200

    Apply HOC via constant instead on export

--- modules/offers/client/components/OfferLocation.component.js
+++ modules/offers/client/components/OfferLocation.component.js
@@ -1,13 +1,13 @@
 // External dependencies
 import { Marker } from 'react-map-gl';
 import PropTypes from 'prop-types';
-import React from 'react';
+import React, { Component } from 'react';
 import ReactMapGL from 'react-map-gl';
 
 // Internal dependencies
 import withMapControls from '@/modules/core/client/components/withMapControls';
 
-class OfferLocation extends React.Component {
+class OfferLocation extends Component {
   constructor(props) {
     super(props);
 
@@ -26,12 +26,15 @@ class OfferLocation extends React.Component {
     this.setState(viewport);
   }
 
+  MapWithControls = withMapControls(<ReactMapGL/>);
+
   render() {
+    const { MapWithControls } = this;
     const { offer } = this.props;
     const { viewport } = this.state;
 
     return (
-      <ReactMapGL
+      <MapWithControls
         {...viewport}
         className="offer-location"
         height={ 320 }
@@ -44,7 +47,7 @@ class OfferLocation extends React.Component {
         >
           {status.status}
         </Marker>
-      </ReactMapGL>
+      </MapWithControls>
     );
   }
 }
@@ -53,4 +56,4 @@ OfferLocation.propTypes = {
   offer: PropTypes.object.isRequired
 };
 
-export default withMapControls(OfferLocation);
+export default OfferLocation;
