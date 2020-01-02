20abfd5372a866b3cd004a8b4855e796f3c58f85
Mikael Korpela <mikael@ihminen.org>
2020-01-02 17:05:17 +0200

    Simplify withMapControls HOC

--- modules/core/client/components/withMapControls.js
+++ modules/core/client/components/withMapControls.js
@@ -1,8 +1,7 @@
 // External dependencies
+import '~mapbox-gl/dist/mapbox-gl.css';
 import { NavigationControl } from 'react-map-gl';
-import get from 'lodash/get';
-import React, { Component } from 'react';
-
+import React from 'react';
 
 /**
  * Higher Order Component that configures Trustroots map layers:
@@ -10,41 +9,27 @@ import React, { Component } from 'react';
  * - Adds Navigation control
  */
 const withMapControls = (WrappedComponent) => {
-  // @TODO: what's up with this?
-  // eslint-disable-next-line react/display-name
-  return class extends Component {
-    constructor(props) {
-      super(props);
-      this.state = {
-        config: get(window, ['settings', 'mapbox'], {})
-      };
-    }
+  const publicKey = window?.settings?.mapbox?.publicKey;
 
-    render() {
-      const { config } = this.state;
-      const { publicKey } = config;
+  if (!publicKey) {
+    return (
+      <div>You must configure Mapbox <code>publicKey</code> for maps to work.</div>
+    );
+  }
 
-      if (!publicKey) {
-        return <div>You must configure Mapbox <code>publicKey</code> for maps to work.</div>;
-      }
-
-      // style="mapbox://styles/mapbox/streets-v9"
-      // https://uber.github.io/react-map-gl/#/Documentation/api-reference/interactive-map
-      return (
-        <WrappedComponent
-          dragRotate={false}
-          mapboxApiAccessToken={publicKey}
-          touchRotate={false}
-          {...this.props}
-        >
-          <div className="map-navigation-control-container">
-            <NavigationControl showCompass={false} />
-          </div>
-          {this.children}
-        </WrappedComponent>
-      );
-    }
-  };
+  return (
+    <WrappedComponent
+      dragRotate={false}
+      mapboxApiAccessToken={publicKey}
+      touchRotate={false}
+      {...this.props}
+    >
+      <div className="map-navigation-control-container">
+        <NavigationControl showCompass={false} />
+      </div>
+      {this.children}
+    </WrappedComponent>
+  );
 };
 
 withMapControls.propTypes = {};
